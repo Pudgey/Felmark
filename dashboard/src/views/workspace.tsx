e51427e7e323bfdc670991acf62f4d5f18c7f862
Donte Ennis <donteennis@Mac.lan>
2026-04-01 22:33:20 -0400

    Make workspace view fill routed width

--- dashboard/src/views/workspace.tsx
+++ dashboard/src/views/workspace.tsx
@@ -5,7 +5,7 @@ import WorkspaceSidebar from "@/components/workspace/sidebar/WorkspaceSidebar";
 
 export default function WorkspaceView() {
   return (
-    <div style={{ display: "flex", height: "100%", overflow: "hidden" }}>
+    <div style={{ display: "flex", flex: 1, width: "100%", minWidth: 0, height: "100%", overflow: "hidden" }}>
       <WorkspaceSidebar />
       <div style={{ flex: 1, minWidth: 0 }}>
         <Canvas />
